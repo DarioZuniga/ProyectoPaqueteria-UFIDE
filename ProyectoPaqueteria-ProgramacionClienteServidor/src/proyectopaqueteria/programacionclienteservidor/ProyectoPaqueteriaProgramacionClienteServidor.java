7b92983e8ddcc9c32a56edba4a6118c52c159cde
Jose Dario Zuniga Gamboa <93889463+DarioZuniga@users.noreply.github.com>
2022-03-16 17:51:55 -0600

    1

---
+++ ProyectoPaqueteria-ProgramacionClienteServidor/src/proyectopaqueteria/programacionclienteservidor/ProyectoPaqueteriaProgramacionClienteServidor.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto Cliente-Servidor
+-------------------------Universidad Fidelitas-----------------------
+ * Jose Dario Zuniga G.
+ *
+ *
+ */
+package proyectopaqueteria.programacionclienteservidor;
+
+/**
+ *
+ * @author josed
+ */
+public class ProyectoPaqueteriaProgramacionClienteServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int ifa;
+         int jhg;
+    }
+    
+}
